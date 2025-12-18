f389831de79fef2b6fc847789ed81ab66f564e43
Rikhav21 <167136725+Rikhav21@users.noreply.github.com>
2025-12-17 23:04:31 -0500

    changed binding port

--- main.py
+++ main.py
@@ -94,4 +94,4 @@ def complete_gift(gift_id):
     return '', 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
